b4d6c900d7dc9936d5594f4ee79698dad931d553
gviejo <guillaume.viejo@gmail.com>
2019-02-15 15:46:01 -0500

    starting...

---
+++ python/main_A1406.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pands as pd
+import neuroseries as nts
+from pylab import *
+from wrappers import *
+from functions import *
+
+data_directory = '/mnt/DataGuillaume/LMN/A1406/'
+
