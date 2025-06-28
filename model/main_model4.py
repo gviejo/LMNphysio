8dcfd7c37afdd434b42c9b0ccfc46bceb54123a1
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-06-28 18:21:01 -0400

    update

--- model/main_model4.py
+++ model/main_model4.py
@@ -2,11 +2,11 @@
 # @Author: Guillaume Viejo
 # @Date:   2025-06-19 15:28:18
 # @Last Modified by:   Guillaume Viejo
-# @Last Modified time: 2025-06-26 15:47:44
+# @Last Modified time: 2025-06-28 18:19:19
 """
-First model of the paper 
-LMN -> ADN 
-Non linearity + inhibition in ADN
+N LMN -> N ADN 
+Non linearity + CAN Current + inhibition in ADN + PSB Feedback
+Population coherence
 
 """
 
@@ -15,23 +15,32 @@ from matplotlib.pyplot import *
 from scipy.ndimage import gaussian_filter, gaussian_filter1d
 from sklearn.manifold import Isomap
 from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import StandardScaler
 from scipy.stats import pearsonr
 
 from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 from numba import jit, njit
 
-def make_LMN_weights(N, sigma=100):
-	x = np.arange(-N//2, N//2)
-	y = np.exp(-(x**2)/sigma)
-	y = np.tile(y, 2)[N//2:-N//2]
-	w = np.zeros((N,N))
-	for i in range(N):
-		w[i] = np.roll(y, i)
-	return w
+@njit
+def make_PSB_LMN_weights(N, M, sigma=100.0):
+    x = np.arange(-N//2, N//2)
+    y = np.exp(-(x * x) / sigma)
+    
+    # Manual tiling replacement: concatenate y with itself
+    y_tiled = np.concatenate((y, y))
+    
+    # Slice the middle portion
+    y = y_tiled[N//2:-N//2]
+    
+    w = np.zeros((N, N))
+    for i in range(N):
+        w[i] = np.roll(y, i)
+    
+    return w
 
 
 @njit
-def make_LMN_weights(N, sigma=100.0):
+def make_ADN_LMN_weights(N, M, sigma=100.0):
     x = np.arange(-N//2, N//2)
     y = np.exp(-(x * x) / sigma)
     
@@ -48,192 +57,219 @@ def make_LMN_weights(N, sigma=100.0):
     return w
 
 @njit
-def sigmoide(x, beta=10, thr=1):
-	return 1/(1+np.exp(-(x-thr)*beta))
-
-# @njit
-def run_network(w_lmn_lmn, noise_lmn_, 
-				w_lmn_adn_, noise_adn_, 
-				w_adn_trn_, w_trn_adn_, thr_adn, N_t=4000):
-	tau = 0.1
-	N_lmn = 60
-	N_adn = 60
-	N_psb = 60
-
-	phase = np.linspace(0, 2*np.pi*(N_t//200), N_t)%(2*np.pi)
-	idx = np.digitize(phase, np.linspace(0, 2*np.pi, N_lmn))
-	# phase[N_t//3:] = 0
-
-	#############################
-	# LMN
-	#############################
-	inp_lmn = np.zeros((N_t, N_lmn))
-	for i in range(N_t):
-		inp_lmn[i,idx[i]] = 1
-	w_lmn = make_LMN_weights(N_lmn, 10)*w_lmn_lmn
-	noise_lmn = np.random.randn(N_t, N_lmn)*noise_lmn_
-	r_lmn = np.zeros((N_t, N_lmn))
-	x_lmn = np.zeros(N_lmn)
-
-
-	#############################
-	# ADN
-	#############################
-	w_lmn_adn = w_lmn_adn_
-	noise_adn = np.random.randn(N_t, N_adn)*noise_adn_
-	r_adn = np.zeros((N_t, N_adn))
-	# x_adn = np.random.randn(N_adn)
-	x_adn = np.zeros(N_adn)
-	x_cal = np.zeros(N_adn)
-	I_ext = np.zeros((N_t, N_adn))
-
-	#############################
-	# TRN
-	#############################
-	r_trn = np.zeros((N_t))
-	x_trn = 0
-	w_adn_trn = w_adn_trn_
-	w_trn_adn = w_trn_adn_
-
-
-	alpha = 2
-	beta = 0
-
-
-	###########################
-	# MAIN LOOP
-	###########################
-
-	for i in range(N_t):
-
-		# REmoving driver	
-		if i == N_t//2:
-			alpha = 0.0
-			beta= 1.0
-
-		# LMN
-		r_lmn[i] = np.maximum(0, np.tanh(x_lmn))
-		x_lmn = x_lmn + tau * (
-			-x_lmn 
-			# + np.dot(w_lmn, r_lmn[i])
-			# + np.dot(w_psb_lmn, r_psb[i])*beta
-			+ noise_lmn[i]
-			+ inp_lmn[i]*alpha
-			+ beta/N_lmn
-			)
-
-		# ADN
-		r_adn[i] = np.maximum(0, np.tanh(x_adn))
-		# TRN
-		r_trn[i] = np.maximum(0, np.tanh(x_trn))
-
-		# Calcium
-		x_cal = x_cal + tau * (
-			- x_cal
-			+ r_adn[i] * 3
-			)
-
-		# I_ext[i] = sigmoide(x_cal+r_lmn[i]*w_lmn_adn-r_trn[i], 10, thr_adn) + noise_adn[i]
-		I_ext[i] = np.maximum(0, np.tanh(x_cal+r_lmn[i]*w_lmn_adn-r_trn[i])) + noise_adn[i]
-
-		x_adn = x_adn + tau * (
-			- x_adn
-			+ I_ext[i]
-			# + sigmoide(I_ext[i], 7, thr_adn)
-			# + (1/(1+np.exp(-(I_ext-thr_adn)*5)))
-			# + ( 1/(1+np.exp(-(r_lmn[i]-thr_adn)*5)))*w_lmn_adn
-			# + noise_adn[i]
-			# - r_trn[i]
-			# + x_cal[i]
-			)
-		x_trn = x_trn + tau * (
-			-x_trn
-			+ np.sum(r_adn[i])*w_adn_trn
-			)
-
-	return (r_lmn, r_adn, r_trn, I_ext)
-
-
-N_t = 6000
+def sigmoide(x, beta=50, thr=1):
+    return 1/(1+np.exp(-(x-thr)*beta))
 
 
 
-r_lmn, r_adn, r_trn, I_ext = run_network(
-	w_lmn_lmn=0.0,
-	noise_lmn_=1,
-	w_lmn_adn_=1,
-	noise_adn_=1,
-	w_adn_trn_=50,
-	w_trn_adn_=50,
-	thr_adn=3,
-	N_t=N_t
-	)
+tau = 0.1
+N_lmn = 32
+N_adn = 64
+
+noise_lmn_=0.5
+noise_adn_=0.1
+noise_cal_=0.1
+
+w_lmn_adn_=1
+w_adn_trn_=1
+w_trn_adn_=1
+w_psb_lmn_=1
+
+thr_adn=1.5
+thr_cal=1.0
+thr_shu=1.0
+
+D_lmn = 0.1
+
+N_t=30000
+
+
+phase = np.linspace(0, 2*np.pi*(N_t//200), N_t)%(2*np.pi)
+idx = np.digitize(phase, np.linspace(0, 2*np.pi, N_lmn+1))
+if idx.min() == 1:
+    idx -= 1
+
+offset = 100
+duration = 10000
+slices = [
+    slice(offset, duration*1), #Wakefulness
+    slice(duration*1+offset, duration*2), # Full model
+    slice(duration*2+offset, duration*3), # Without PSB Feedback            
+        ]
+alpha = 4.0 # Wakefulness -> Sleep
+beta = 1.0 # OPTO PSB Feedback
+
+#############################
+# LMN
+#############################
+inp_lmn = np.zeros((N_t, N_lmn))
+for i in range(N_t):
+    inp_lmn[i,idx[i]] = 1.0
+noise_lmn = np.random.randn(N_t, N_lmn)*noise_lmn_
+r_lmn = np.zeros((N_t, N_lmn))
+x_lmn = np.zeros((N_t, N_lmn))
+
+
+#############################
+# ADN
+#############################
+w_lmn_adn = w_lmn_adn_
+noise_adn =  np.random.randn(N_t, N_adn)*noise_adn_
+noise_cal =  np.random.randn(N_t, N_adn)*noise_cal_
+r_adn = np.zeros((N_t, N_adn))
+x_adn = np.zeros((N_t, N_adn))
+x_cal = np.zeros((N_t, N_adn))
+I_ext = np.zeros((N_t, N_adn))
+
+
+#############################
+# TRN
+#############################
+r_trn = np.zeros((N_t))
+x_trn = np.zeros((N_t))
+w_adn_trn = w_adn_trn_
+w_trn_adn = w_trn_adn_
+
+
+############################
+# PSB FEEDback
+############################
+w_psb_lmn = make_PSB_LMN_weights(N_lmn, N_adn, 50)*w_psb_lmn_
+
+
+
+
+###########################
+# MAIN LOOP
+###########################
+
+for i in range(1, N_t):
+
+    if i == slices[0].stop:
+        alpha = 0.0
+        D_lmn = 1.1
+    if i == slices[1].stop:
+        beta = 0.0
+        D_lmn = 1.1
+
+
+    I_lmn = np.dot(w_psb_lmn, r_adn[i-1]) + inp_lmn[i] * alpha
+
+    # LMN
+    x_lmn[i] = x_lmn[i-1] + tau * (
+        -x_lmn[i-1] 
+        + noise_lmn[i]
+        + I_lmn * beta
+        + D_lmn
+        )
+    r_lmn[i] = np.maximum(0, x_lmn[i])
+
+    # ADN
+    I_ext[i] = r_lmn[i]*w_lmn_adn - r_trn[i-1] * w_trn_adn
+
+
+    # Calcium
+    x_cal[i] = x_cal[i-1] + tau * (
+        - x_cal[i-1]
+        + sigmoide(x_adn[i-1], thr=thr_cal)
+        + noise_cal[i]
+        )
+
+    
+    x_adn[i] = x_adn[i-1] + tau * (
+        - x_adn[i-1]
+        + I_ext[i]
+        + noise_adn[i]
+        + sigmoide(-x_cal[i], thr=-thr_shu)
+        )
+
+    r_adn[i] = sigmoide(x_adn[i], thr=thr_adn)
+
+    # TRN
+    x_trn[i] = x_trn[i-1] + tau * (
+        -x_trn[i-1]
+        + np.sum(r_adn[i])*w_adn_trn
+        )
+
+    # r_trn[i] = np.maximum(0, np.tanh(x_trn[i]))
+    r_trn[i] = np.maximum(0, x_trn[i])
+
 
 
 imap = {}
 popcoh = {}
 
-# for i in range(100,N_t,N_t//3):
-# 	imap[i] = {}
+
 for k, r in zip(['lmn', 'adn'],[r_lmn, r_adn]):
-	imap[k] = {}
-	popcoh[k] = {}
-	for i, sl in enumerate([slice(100, N_t//2), slice(N_t//2+1000, N_t)]):
-	
-		# sum_ = r[sl].sum(1)
-		# idx = sum_>np.percentile(sum_, 10)
-		# tmp = gaussian_filter1d(r[sl], 1)
-		tmp = r[sl]
-		# tmp = tmp[tmp.mean(1) > np.percentile(np.mean(tmp, 1), 50)]
+    imap[k] = {}
+    popcoh[k] = {}
+    for i, sl in enumerate(slices):
+    
+        # sum_ = r[sl].sum(1)
+        # idx = sum_>np.percentile(sum_, 10)
+        tmp = gaussian_filter(r[sl], sigma=1, order=(0,1))
+        # tmp = r[sl]
+        tmp = StandardScaler().fit_transform(tmp)
+        # tmp = r[sl]        
+        # tmp = tmp[tmp.mean(1) > np.percentile(np.mean(tmp, 1), 50)]
 
-		imap[k][i] = KernelPCA(n_components=2, kernel='cosine').fit_transform(tmp)
+        # imap[k][i] = KernelPCA(n_components=2, kernel='cosine').fit_transform(tmp)
 
-		tmp = np.corrcoef(r[sl].T)
-		popcoh[k][i] = tmp[np.triu_indices(tmp.shape[0], 1)]
-
-
-
-
-
+        # tmp = np.corrcoef(r[sl].T)
+        tmp = np.corrcoef(tmp.T)
+        popcoh[k][i] = tmp[np.triu_indices(tmp.shape[0], 1)]
 
 
-figure(figsize=(40, 15))
-subplot(411)
-imshow(r_adn.T, aspect='auto')
-title("ADN")
-subplot(412)
-imshow(r_lmn.T, aspect='auto')
-title("LMN")
-subplot(414)
-imshow(I_ext.T, aspect='auto')
-title("I_ext")
-subplot(413)
-plot(r_lmn.sum(1), 'o-', label="LMN")
-plot(r_adn.sum(1), 'o-', label="ADN")
-plot(r_trn, 'o-', label="TRN")
-legend()
-tight_layout()
 
-# figure(figsize=(40, 15))
-# count = 0
-# for i in range(2):
-# 	for j, k in enumerate(['lmn', 'adn']):
-# 		subplot(2,2,count+1)
-# 		count += 1
-# 		scatter(imap[k][i][:,0], imap[k][i][:,1])
-# 		title(k)
-# 		if i == 0:
-# 			ylabel(['wake', 'sleep'][i])
+figure()
+gs = GridSpec(2,2)
+for i, st in enumerate(['lmn', 'adn']):
+    for j, k in enumerate([1,2]):
+        subplot(gs[j,i])
+        plot(popcoh[st][0], popcoh[st][k], 'o')
+        r, p = pearsonr(popcoh[st][0], popcoh[st][k])
+        m, b = np.polyfit(popcoh[st][0], popcoh[st][k], 1)
+        x = np.linspace(popcoh[st][0].min(), popcoh[st][0].max(),5)
+        plot(x, x*m + b)
+
+        xlabel("wake")
+        if j == 0:
+            ylabel("Full sleep")
+        if j == 0:
+            ylabel("No PSB feedback")
+        title(st + " " + f"r={np.round(r,2)}")
 
 
-# figure()
-# subplot(121)
-# plot(popcoh['lmn'][0], popcoh['lmn'][1], 'o')
-# title("r="+str(np.round(pearsonr(popcoh['lmn'][0], popcoh['lmn'][1])[0], 3)))
-# subplot(122)
-# plot(popcoh['adn'][0], popcoh['adn'][1], 'o')
-# title("r="+str(np.round(pearsonr(popcoh['adn'][0], popcoh['adn'][1])[0], 3)))
+
+figure()
+n_rows = 6
+ax = subplot(n_rows,1,1)
+# plot(r_lmn, '-')
+pcolormesh(r_lmn.T, cmap='jet')#, vmin=0.9)
+ylabel("r_lmn")
+ax = subplot(n_rows,1,2, sharex=ax)
+plot(x_adn, '-')
+axhline(thr_adn, linestyle='--')
+axhline(thr_cal)
+ylabel("x_adn")
+subplot(n_rows,1,3,sharex=ax)
+plot(x_cal, '-')
+axhline(thr_shu)
+ylabel("X_cal")
+subplot(n_rows,1,4, sharex=ax)
+plot(r_adn, '-')
+ylabel("r_adn")
+subplot(n_rows,1,5, sharex=ax)
+pcolormesh(r_adn.T, cmap='jet')
+ylabel("r_adn")
+subplot(n_rows,1,6, sharex=ax)
+plot(r_trn, '-', color='red')
+plot(x_trn, '--', color='gray')
+ylabel("r_trn")
+
 show()
+
+
 
 
 
